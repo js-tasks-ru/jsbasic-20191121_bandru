4ab2f3e2ee34935a0fdb9278d606f773f7286aa2
bandru <beanu@yandex.ru>
2019-12-09 12:58:23 +0400

    Выполнил задание 2.1 - клонирование объекта

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,13 @@
  * @returns {Object}
  */
 function clone(obj) {
+  let resObj = {};
+  for (let key in obj) {
+    if (typeof (obj[key]) === 'object' && obj[key] !== null) {
+      resObj[key] = clone(obj[key]);
+    } else {
+      resObj[key] = obj[key];
+    }
+  }
+  return resObj;
 }
