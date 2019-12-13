556af3f8808e5affd81afb4efbd095f3e67619a5
bandru <beanu@yandex.ru>
2019-12-13 20:38:22 +0400

    Выполнил задание 3.2 - Найти min/max в строке

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,11 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let resArray1 = str.split(' ');
+  let resArray2 = [];
+  for (let elem of resArray1) {
+    resArray2 = resArray2.concat(elem.split(','));
+  }
+  resArray2.sort((a, b)=> a - b);
+  return {min: +resArray2[0], max: +resArray2[resArray2.length - 1]};
 }
