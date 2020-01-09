5c77ba3bb69cfcb0630ff94425aa7b95343c3936
bandru <beanu@yandex.ru>
2020-01-09 16:44:55 +0400

    Задание 4 - Функция makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,9 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let ul = document.createElement('ul');
+  for (let friend of friends) {
+    ul.insertAdjacentHTML('beforeend', `<li>${friend.firstName} ${friend.lastName}</li>`);
+  }
+  return ul;
 }
